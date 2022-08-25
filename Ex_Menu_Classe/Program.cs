56094281349d46b3b7e020eac1fcda0f189fdb50
artur <artur@DESKTOP-V0JU5PP>
2022-08-25 11:45:43 -0300

    Add project files.

---
+++ Ex_Menu_Classe/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Ex_Menu_Classe
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa[] contatos = new Pessoa[10];
+            int opcao;
+
+            do
+            {
+                Console.WriteLine("1 - CADASTRAR\n0 - SAIR");
+                Console.Write("Informe a ocao desejada: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Tchau!");
+                        break;
+
+                    case 1:
+
+                        break;
+                }
+
+            } while (opcao != 0);
+
+            Console.WriteLine(">>>>FIM<<<<");
+        }
+    }
+}
