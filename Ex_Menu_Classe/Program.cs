c04f8114409a774ded9e0a988f32bba82c6f4207
artur <artur@DESKTOP-V0JU5PP>
2022-08-25 11:57:30 -0300

    save

--- Ex_Menu_Classe/Program.cs
+++ Ex_Menu_Classe/Program.cs
@@ -12,7 +12,7 @@ namespace Ex_Menu_Classe
             do
             {
                 Console.WriteLine("1 - CADASTRAR\n0 - SAIR");
-                Console.Write("Informe a ocao desejada: ");
+                Console.Write("Informe a opcao desejada: ");
                 opcao = int.Parse(Console.ReadLine());
 
                 switch (opcao)
@@ -22,13 +22,40 @@ namespace Ex_Menu_Classe
                         break;
 
                     case 1:
-
+                        contatos[0] = lerPessoa();
                         break;
                 }
 
             } while (opcao != 0);
 
             Console.WriteLine(">>>>FIM<<<<");
+        }
+
+        static Pessoa lerPessoa()
+        {
+            string nome;
+            int idade;
+
+            Console.Clear();
+
+            Console.Write("Informe o nome da pessoa: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Informe a idade: ");
+            idade = int.Parse(Console.ReadLine());
+
+            return new Pessoa
+            {
+                Nome = nome,
+                Idade = idade
+            };
+
+            //Pessoa pessoa = new Pessoa
+            //{
+            //    Nome = nome,
+            //    Idade = idade
+            //};
+            //return pessoa;
         }
     }
 }
